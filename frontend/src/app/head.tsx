e9590727063598ced846c6db936d042dc68c9626
tbanerj <trinavbanerjee7@gmail.com>
2025-08-06 15:40:03 -0700

    adding app metas

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>PitchPanel</title>
+      <meta name="description" content="The Next Generation of Vocal Training" />
+      <meta name="apple-itunes-app" content="app-id=1234567890" />
+    </>
+  );
+}
